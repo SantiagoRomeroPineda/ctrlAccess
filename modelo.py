5de06ea7894223bc039f98944321f4811849ab91
Santiago <saropi9978@gmail.com>
2020-08-17 18:10:08 -0500

    code completed

--- modelo.py
+++ modelo.py
@@ -13,7 +13,8 @@ class Modelo(object):
         c.execute(sql,parametros)
         if tipo:
             return dict(c.fetchall()[0])
-        self.conn.commit() 
+        self.conn.commit()
+        return c.lastrowid() 
 
     def getUsuario(self, documento):
          sql= "select id_usuario, permiso from usuario where documento = ?"
@@ -23,7 +24,7 @@ class Modelo(object):
     def insertUsuario(self,data):
         sql= "insert into usuario (documento, nombres, apellidos, genero, fecha_nacimiento, permiso) values (?, ?, ?, ?, ?, ?)"
         parametros=(data["documento"],data["nombres"],data["apellidos"],data["genero"],data["fecha_nacimiento"],data["permiso"],)
-        self.query(sql,parametros)
+        return self.query(sql,parametros)
 
     def updateUsuario(self, data):
         sql= "update usuario set permiso = ? where documento =?"
@@ -33,7 +34,7 @@ class Modelo(object):
     def insertRegistro(self, data):
         sql= "insert into registro (id_usuario, fecha_registro, temperatura) values (?, ?, ?)"
         parametros= (data["id_usuario"],data["fecha_registro"],data["temperatura"],)
-        self.query(sql, parametros)
+        return self.query(sql, parametros)
 
 
 
