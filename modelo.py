58e33ada1f24532c276741084f96a8023f1df14e
Santiago <saropi9978@gmail.com>
2020-08-17 19:33:45 -0500

    -proyecto finalizado

--- modelo.py
+++ modelo.py
@@ -2,7 +2,7 @@ import sqlite3
 
 class Modelo(object):
     def __init__(self):
-        self.conn= sqlite3.connect('/run/user/1000/gvfs/sftp:host=192.168.0.26,user=pi/home/pi/Documents/proyectos/proyectoSL/db/tablas.db')
+        self.conn= sqlite3.connect('/home/pi/Documents/proyectos/proyectoSL_update/ctrlAccess/tablas.db')
         self.conn.row_factory= sqlite3.Row
 
     def closeConection(self):
@@ -12,15 +12,17 @@ class Modelo(object):
         c = self.conn.cursor()
         c.execute(sql,parametros)
         if tipo:
-            return dict(c.fetchall()[0])
+            a= c.fetchall()
+            return (None if not a  else dict(a[0]))   
+        
         self.conn.commit()
-        return c.lastrowid() 
+        return c.lastrowid
 
     def getUsuario(self, documento):
          sql= "select id_usuario, permiso from usuario where documento = ?"
          parametros=(documento,)
          return self.query(sql,parametros,True)
-    
+
     def insertUsuario(self,data):
         sql= "insert into usuario (documento, nombres, apellidos, genero, fecha_nacimiento, permiso) values (?, ?, ?, ?, ?, ?)"
         parametros=(data["documento"],data["nombres"],data["apellidos"],data["genero"],data["fecha_nacimiento"],data["permiso"],)
@@ -30,12 +32,11 @@ class Modelo(object):
         sql= "update usuario set permiso = ? where documento =?"
         paramteros=(data["documento"],data["permiso"],)
         self.query(sql,paramteros)
-    
+
     def insertRegistro(self, data):
         sql= "insert into registro (id_usuario, fecha_registro, temperatura) values (?, ?, ?)"
         parametros= (data["id_usuario"],data["fecha_registro"],data["temperatura"],)
         return self.query(sql, parametros)
-
 
 
 
